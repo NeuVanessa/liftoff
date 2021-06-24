aaf4cfde5e4d370612d0fcf71c8b37de5934b80c
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-23 22:32:51 -0300

    adicionando bacground gradient ✨

--- App.tsx
+++ App.tsx
@@ -2,23 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./src/screens/Login";
-import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
-import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Rajdhani_500Medium,
-    Rajdhani_700Bold
+    Rajdhani_700Bold,
   });
 
-  if(!fontsLoaded){
-    return <AppLoading/>
+  //Enquando as fontes não for carregada a tela de login não aparecerá
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-  return <Login />
+  return <Login />;
 }
 
 const styles = StyleSheet.create({
