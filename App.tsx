dc51c896459e76116995c3c5b95d010eeab4d1c6
Vanessa Lopes <63751937+NeuVanessa@users.noreply.github.com>
2021-06-23 22:27:51 -0300

    importando as fonts no App.tsx ✨

--- App.tsx
+++ App.tsx
@@ -2,8 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./src/screens/Login";
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
   return <Login />
 }
 
